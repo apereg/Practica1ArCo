f730f51bb5917da1add0fc2daa824c10e28a8eb1
Adri <apereg24@estudiantes.unileon.es>
2020-02-27 01:26:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,47 @@
 #include <iostream>
 
-#include <stdlib.h>
+using namespace std;
 
-void crearElementos(Profesor profesor1, Profesor profesor2);
-int main(int argc, char *argv[])
-{
+class Persona{
+protected:
+	String nombre;
+	String dni;
+	public:
+		void getNombre(){
 
+		}
+
+		void getdni(){
+
+		}
+};
+
+class Alumno : public Persona{
+private:
+	int nota1;
+	int nota2;
+	int nota3;
+	int notaMedia;
+public:
+	Alumno(String nombrein, String dniin):Persona(nombrein, dniin){
+
+	}
+	Alumno(String nombrein, String dniin, int nota1):Persona(nombrein, dniin){
+		
+	}
+	Alumno(String nombrein, String dniin, int nota1,int nota2):Persona(nombrein, dniin){
+		
+	}
+	Alumno(String nombrein, String dniin, int nota1, int nota2, int nota3):Persona(nombrein, dniin){
+		
+	}
+
+	void setNota(int nota){
+
+	}
+};
+
+int main(int argc, char *argv[]){
+	
     return a.exec();
 }
